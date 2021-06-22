c586a1aa02baea748ad436ff7b5cb182fc1ada7d
nandini-django-2021 <85300660+nandini-django-2021@users.noreply.github.com>
2021-06-22 14:03:34 +0530

    Update 0001_initial.py

--- Django skills/features/migrations/0001_initial.py
+++ Django skills/features/migrations/0001_initial.py
@@ -15,9 +15,9 @@ class Migration(migrations.Migration):
             name='Info',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=400)),
-                ('contact', models.CharField(max_length=400)),
-                ('interests', models.CharField(max_length=400)),
+                ('name', models.CharField(max_length=50)),
+                ('contact', models.CharField(max_length=40)),
+                ('interests', models.CharField(max_length=30)),
             ],
         ),
     ]
